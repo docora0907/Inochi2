1878188d736ac0fcd6932b07f39895dfbf4da97f
docora0907 <koto001746@gmail.com>
2025-09-16 14:22:23 +0900

    0916/1422

---
+++ Assets/Scripts/PhotoChange.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoChange : MonoBehaviour
+{
+    private Image img;
+    private int count = 1;
+
+    void Start()
+    {
+        img = GameObject.Find("Image").GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (count < 3)
+                count++;
+            else
+                count = 1;
+
+            img.sprite = Resources.Load<Sprite>("Image/photo" + count.ToString());
+        }
+    }
+}

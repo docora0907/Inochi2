df73e712c76d659c2a67157045443bc8fe02d0ca
saya-cham <sa850arashi@gmail.com>
2025-09-19 23:42:13 +0900

    0919/2342

--- Assets/Scripts/SwitchScript.cs
+++ Assets/Scripts/SwitchScript.cs
@@ -1,9 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class SwitchScript : MonoBehaviour
+using UnityEngine.SceneManagement;
+
+public class SwichScene : MonoBehaviour
 {
-    private void OnMouseDown()
+
+    public string sceneName;
+    public string objectName1;
+    public string objectName2;
+    GameObject showObject;
+    GameObject swich;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        
+        swich = GameObject.Find(objectName2);
+        showObject = GameObject.Find(objectName1);
+        showObject.SetActive(false);
     }
+
+    void OnMouseDown()
+    {
+        this.swich.SetActive(false);
+        this.showObject.SetActive(true);
+        SceneManager.LoadScene(sceneName);
+    }
+
 }

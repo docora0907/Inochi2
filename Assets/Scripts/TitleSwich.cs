2b138102721e1ed9a6fecf8779050966ba78a09f
docora0907 <koto001746@gmail.com>
2025-09-26 18:40:56 +0900

    0926/1840

---
+++ Assets/Scripts/TitleSwich.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TitleSwich : MonoBehaviour
+{
+    public GameObject darkRoom;
+
+    //public GameObject ok;
+
+    public float y;
+    public GameObject ca;
+
+    void Start()
+    {
+        darkRoom.SetActive(false);
+        //ok.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        darkRoom.SetActive(true);
+        //ok.SetActive(true);
+
+        ca.transform.position = new Vector3(transform.position.x, y, -10);
+    }
+}

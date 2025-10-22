f422d5569bceae608bb7988bd1d11bd457ba78c4
yamasa-4 <masaeyamaboshi4@gmail.com>
2025-10-22 21:54:11 +0900

    1022/2154

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,6 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
+    public Text timeTexts;
+    public Text CountText;
+    float totalTime = 10;
+    int retime;
+    float countdown = 3f;
+    int count;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +20,23 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(countdown >= 0)
+        {
+            countdown -= Time.deltaTime;
+            count = (int)countdown;
+            CountText.text = count.ToString();
+        }
+        if(countdown <= 0)
+        {
+            CountText.text = "";
+            totalTime -= Time.deltaTime;
+            retime = (int)totalTime;
+            timeTexts.text = retime.ToString();
+            if(retime == 0)
+            {
+                SceneManager.LoadScene("result");
+            }
+        }
         
     }
 }
